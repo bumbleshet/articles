656414e03923a88705270360773c63f606ef12f4
bumbleshet <guarosidney@gmail.com>
2018-12-11 19:41:39 +0800

    added pre

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,25 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
+<h2>Title: {{$article->title}}</h2>
+	@include('articles._error')
+	{!! Form::model($article, ['method' => 'put']) !!}
 
-	@if(count($errors))
-		<div class="alert alert-danger">
-			@foreach($errors->all() as $error)
-				{{ $error }} <br>
-			@endforeach
-		</div>
-	@endif
-
-	{!! Form::open() !!}
-
-		{!! Form::label('title', 'Title') !!}
-		{!! Form::text('title', $article->title, ['class' => 'form-control', 'placeholder' => 'ex. Article Title']) !!}
-
-		<div class="form-group" >
-			{!! Form::label('body', 'Body:') !!}
-			{!! Form::textarea('body', $article->body, ['class' => 'form-control']) !!}
-		</div>
-		{!! Form::submit('Save Changes', ['class' => 'btn btn-primary']) !!}
+		@include('articles._form', ['btnText' => 'save changes']))
 
 	{!! Form::close() !!}
 @stop
